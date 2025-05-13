d6fa110d051909cea1a1be68e969c8bd97e2a866
Rafael Matias <126630910+RafaelMatiass@users.noreply.github.com>
2025-05-13 20:13:43 -0300

    Update FilmeDAO.java
    Implemented FilmeDAO

--- projeto/src/main/java/br/edu/ifsp/arq/dao/FilmeDAO.java
+++ projeto/src/main/java/br/edu/ifsp/arq/dao/FilmeDAO.java
@@ -1,6 +1,70 @@
 package br.edu.ifsp.arq.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import br.edu.ifsp.arq.model.Filme;
+
 public class FilmeDAO {
-	//Colocar RemoveFilme, GetFilme, UpdateFilme, AddFilme
-	//Está nas outras classes
+
+	private static FilmeDAO instance = null;
+	private List<Filme> filmes;
+	private int proximoId;
+	
+	private FilmeDAO() {
+	    filmes = new ArrayList<>();
+	    proximoId = 1;
+	}
+	
+	public static FilmeDAO getInstance() {
+	    if (instance == null) {
+	        instance = new FilmeDAO();
+	    }
+	    return instance;
+	}
+	
+	public boolean adicionarFilme(Filme filme) {
+	    filme.setId(proximoId++);
+	    return filmes.add(filme);
+	}
+	
+	public List<Filme> getFilmes() {
+	    return filmes;
+	}
+	
+	public Filme buscarPorId(int id) {
+	    for (Filme f : filmes) {
+	        if (f.getId() == id) return f;
+	    }
+	    return null;
+	}
+	
+	public boolean removerFilme(int id) {
+	        for (Filme filme : filmes) {
+	            if (filme.getId() == id) {
+	                filmes.remove(filme);
+	                return true;
+	            }
+	        }
+	   return false; 
+	}
+	
+	public boolean atualizarFilme(Filme filmeAtualizado) {
+	    for (Filme f : filmes) {
+	    	if(f.getId() == filmeAtualizado.getId()) {
+	    		f.setTitulo(filmeAtualizado.getTitulo());
+	    		f.setSinopse(filmeAtualizado.getSinopse());
+	    		f.setAnoLancamento(filmeAtualizado.getAnoLancamento());
+	    		f.setIdioma(filmeAtualizado.getIdioma());
+	    		f.setFormato(filmeAtualizado.getFormato());
+	    		f.setDuracao(filmeAtualizado.getDuracao());
+	    		
+	    		f.setImagem(filmeAtualizado.getImagem());
+	    		
+	    		return true;
+	    	}
+	    }
+	    return false;
+	}
 }
+
+
